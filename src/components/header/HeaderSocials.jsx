a41b7319575c91b988263d5c5b205cb28bb2bfc1
IbtihelHT <hattabiibtihel@gmail.com>
2022-06-24 16:35:00 +0100

    changement

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
+import {MdMail} from 'react-icons/md'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"> <BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/ibtihel-hattabi" target="_blank"> <BsLinkedin/></a>
+        <a href="https://github.com/HATTABI-Ibtihel" target="_blank"><BsGithub/></a>
+        <a href="hattabiibtihel@gmail.com" target="_blank"><MdMail/></a>
         <a href="https://instagram.com" target="_blank"><BsInstagram/></a>
     </div>
   )
