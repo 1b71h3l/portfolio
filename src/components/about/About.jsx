e0721dbb1248885ce3744c9abb3e86e19f174297
IbtihelHT <hattabiibtihel@gmail.com>
2022-06-25 22:40:39 +0100

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import "./about.css"
+import ME from '../../assets/girll.png'
+import { TbAward } from 'react-icons/tb'
+import { FiUsers } from 'react-icons/fi'
+import { AiOutlineFundProjectionScreen } from 'react-icons/ai'
 
 const about = () => {
   return (
-    <section id='about'>about</section>
+    <section id='about'>
+      <h5>Get to know </h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <TbAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <AiOutlineFundProjectionScreen className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet. Est quidem dolor a quod beatae sed iure tenetur et omnis error. Et error dolore ut minus incidunt in quibusdam deserunt ea neque dolorem.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
