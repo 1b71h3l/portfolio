67bd08cbf385d4a47bde2f58c15d3d8bd1c2045c
IbtihelHT <hattabiibtihel@gmail.com>
2022-06-26 20:17:22 +0100

    experience component css

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './experience.css'
-import {MdLibraryAddCheck} from 'react-icons/md';
+import { MdLibraryAddCheck } from 'react-icons/md';
 
 const experience = () => {
   return (
@@ -10,80 +10,102 @@ const experience = () => {
 
       <div className="container experience__container">
         <div className="experience__frontend">
-         <h3>Frontend Developement</h3>
-          <div className="experience__container">
+          <h3>Frontend Developement</h3>
+          <div className="experience__content">
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>HTML</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>HTML</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>CSS</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>CSS</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>JavaScript</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>JavaScript</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>Bootstrap</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>React</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>React</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             {/* <article className='experience__details'>
-              <MdLibraryAddCheck/>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
               <h4>HTML</h4>
               <small className='texte-light'>Experienced</small>
+              </div>
             </article> */}
           </div>
         </div>
-       {/* End of frontend section  */}
+        {/* End of frontend section  */}
 
         <div className="experience__backend">
-        <h3>Backend Developement</h3>
-          <div className="experience__container">
+          <h3>Backend Developement</h3>
+          <div className="experience__content">
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>Node JS</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>Node JS</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>MySQL</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>MySQL</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>Java</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>Java</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
             <article className='experience__details'>
-              <MdLibraryAddCheck/>
-              <h4>C</h4>
-              <small className='texte-light'>Experienced</small>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
+                <h4>C</h4>
+                <small className='texte-light'>Experienced</small>
+              </div>
             </article>
 
-            {/* <article className='experience__details'>
-              <MdLibraryAddCheck/>
+             {/* <article className='experience__details'>
+              <MdLibraryAddCheck className='experience__details-icon' />
+              <div>
               <h4>HTML</h4>
               <small className='texte-light'>Experienced</small>
+              </div>
             </article> */}
-         </div>
+          </div>
         </div>
       </div>
     </section >
