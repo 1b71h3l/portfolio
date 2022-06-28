3c40e97c4f30fc5b50a7398c5951bccceb3aa4a0
IbtihelHT <hattabiibtihel@gmail.com>
2022-06-28 23:29:06 +0100

    experience component2

--- src/components/portfolio/Portfilio.jsx
+++ src/components/portfolio/Portfilio.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/'
+import IMG1 from '../../assets/1.png'
+import IMG2 from '../../assets/2.png'
+import IMG3 from '../../assets/3.png'
+import IMG4 from '../../assets/4.jpg'
+import IMG5 from '../../assets/5.png'
+import IMG6 from '../../assets/6.jpg'
 
 const portfilio = () => {
   return (
