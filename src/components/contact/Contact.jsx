7bc2722a7e43b69fbc70a5980b6268aff36a34c3
IbtihelHT <hattabiibtihel@gmail.com>
2022-07-07 17:33:30 +0100

    contact component styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,21 +13,21 @@ const contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className='contact__option'>
-            <HiOutlineMail/>
+            <HiOutlineMail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>hattabiibtihel@gmail.com</h5>
             <a href="mailto:hattabiibtihel@gmail.com" target="_blank">Send A Message</a>
           </article>
 
           <article className='contact__option'>
-            <RiMessengerLine/>
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messanger</h4>
             <h5>Ibtihel Ht</h5>
             <a href="https://m.me/ibtihel_ht" target="_blank">Send A Message</a>
           </article>
 
           <article className='contact__option'>
-            <ImWhatsapp/>
+            <ImWhatsapp className='contact__option-icon' />
             <h4>Whatsapp</h4>
             <h5>+213558774703</h5>
             <a href="https://api.whatsapp.com/send?phone=213558774703" target="_blank">Send A Message</a>
@@ -37,7 +37,7 @@ const contact = () => {
         <form action=''>
           <input type="text" name="name" placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required />
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <textarea name="message" rows="7" placeholder='Your Message' required/>
            <button type='submit' className='btn btn-primary'>Send Message</button>
         </form>
       </div>
