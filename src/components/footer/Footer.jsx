48a4da2ca03e33876e3325b312b74da9ab766d3f
IbtihelHT <hattabiibtihel@gmail.com>
2022-07-08 17:19:34 +0100

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FiFacebook} from 'react-icons/fi'
+import { BsInstagram} from 'react-icons/bs'
+import {FiTwitter} from 'react-icons/fi'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+    <a href='#' className='fotter__logo' >Ibtihel</a>
+    <ul className='premalinks'>
+      <li><a href='#'>Home</a></li>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#experience'>Experience</a></li>
+      <li><a href='#services'>Services</a></li>
+      <li><a href='#porfolio'>Porfolio</a></li>
+      <li><a href='#testimonials'>Testimonials</a></li>
+      <li><a href='#contact'>Contact</a></li>
+    </ul>
+
+    <div className="footer__socials">
+      <a href='http://facebook.com'><FiFacebook/></a>
+      <a href='http://instagram.com'><BsInstagram/></a>
+      <a href='http://twitter.com'><FiTwitter/></a>
+    </div>
+
+    <div className="footer__copyright">
+      <small>&copy; Ibtihel All rights reserved</small>
+    </div>
+    </footer>
   )
 }
 
